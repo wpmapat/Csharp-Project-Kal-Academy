0b7c04790c40a3114039781831d344b3770262e0
Madhura Pattekar <Madhura Pattekar>
2017-09-13 22:23:43 -0700

    Add project files.

---
+++ BankApp/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This is about bank account (XML comments)
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        //Properties definations
+
+        /// <summary>
+        ///This hold the account number 
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// this will hold email address
+        /// </summary>
+        public string EmailAddress { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountType { get; set; }
+        public DateTime CreatedDate { get; set; }
+        # endregion
+
+    }
+}
