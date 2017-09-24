13dd8bfc2d945805842ed86466ec44b500695e2e
Madhura Pattekar <madhuracp@hotmail.com>
2017-09-24 14:47:11 -0700

    Inital commit

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -9,7 +9,14 @@ namespace BankApp
     class Program
     {
         static void Main(string[] args)
+
         {
+            var account=Bank.CreateAccount("test@test.com");
+
+           Console.WriteLine($"AN:{account.AccountNumber},EA: {account.EmailAddress},Balance:{account.Balance:C},AT:{account.AccountType}");
+
+            var account2 = Bank.CreateAccount("test2@test.com",initialDeposit:500);
+            Console.WriteLine($"AN:{account2.AccountNumber},EA: {account2.EmailAddress},Balance:{account2.Balance:C},AT:{account2.AccountType}");
 
         }
     }
