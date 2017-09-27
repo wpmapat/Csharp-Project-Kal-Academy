46a0bc105532369530377c9e6daf1c9639961781
Madhura Pattekar <madhuracp@hotmail.com>
2017-09-26 20:39:19 -0700

    commit on 9/26

---
+++ InstantMessagingWindowsApp/ActionManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantMessagingWindowsApp
+{
+    class ActionManager
+    {
+        DBManager dBManager;
+       public ActionManager()
+        {
+            dBManager = new DBManager();
+        }
+
+        #region Methods
+        public void SendMessage(User sender, Account a1, string messageText)
+    {       
+
+    }
+        /// <summary>
+        /// This method will create an account
+        /// </summary>
+        /// <param name="u1"></param>
+        /// <returns></returns>
+        public bool CreateAccount(User u1)
+        {
+            dBManager.CreateAccount(u1);
+            return true;
+            
+        }
+        /// <summary>
+        /// This method will help user to sign in in his account by providing phone number and password. Since there is no database yet, i have hardcoded some of my code
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>. 
+        public User LoginUser(int phoneNumber,string password)
+        {
+            User u1=dBManager.getUser(phoneNumber,password);
+            return u1;
+        }
+        //public void EditAcct(User u)
+        //public void DeleteAcct(User u)
+#endregion
+    }
+}
---
+++ InstantMessagingWindowsApp/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantMessagingWindowsApp
+{
+    class User:Account
+    {
+        public User(string name, int phNumber, string emailAddress, string password)
+        {
+            this.Name = name;
+            this. PhoneNo = phNumber;
+            this.EmailAddress = emailAddress;
+            this.CreatedDate = DateTime.Now;
+            this.Password = password;
+        }
+        #region         
+        
+        /// <summary>
+        /// Phone number of the user
+        /// </summary>
+        public int PhoneNo { get; set; }
+        
+        /// <summary>
+        /// Email Address of the user
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Password which user enters at the login screen
+        /// </summary>
+        public string Password { get; set; }
+        
+
+        
+
+        #endregion
+    }
+}
